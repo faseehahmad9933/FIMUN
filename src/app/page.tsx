71469a004e595c9900ec25b1ab78c4818e73b120
Faseeh Ahmad <faseehkamboh9933@gmail.com>
2025-05-31 15:18:00 +0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,106 @@
+"use client";
 import Image from "next/image";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { motion, useAnimation, useScroll, useTransform, useMotionValueEvent } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { scrollYProgress } = useScroll();
+  const { scrollY } = useScroll()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useMotionValueEvent(scrollY, "change", (latest: number) => {
+    console.log("Page scroll: ", latest)
+  })
+  const [beautyRef, beautyInView] = useInView({
+    threshold: 0.5,
+    triggerOnce: false
+  });
+
+  // Transform scroll progress to animation states
+  const titleScale = useTransform(scrollYProgress, [0, 0.25], [1, 1.2]);
+  const x = useTransform(scrollYProgress, [0, 0.1, 0.4, 0.6, 0.8, 1], [1,-850,-850,-850,-850,-850]);
+  const y = useTransform(scrollYProgress, [0, 0.1, 0.4, 0.6, 0.8, 1], [1,-100,-100,-100,-100,-100]);
+  // const d = useTransform(scrollYProgress, [0, 0.4], [1, 1.2]);
+  // const e = useTransform(scrollYProgress, [0, 0.5], [1, 1.2]);
+
+
+  const imageScale = useTransform(scrollYProgress, [0.25, 0.5,], [1, 1.1]);
+  const textOpacity = useTransform(scrollYProgress, [0.5, 0.75], [1, 0.8]);
+  const titleRotation = useTransform(scrollYProgress, [0.75, 1], [0, 5]);
+
+  const [effectText] = useTypewriter({
+    words: [
+      "C.E.O of Pinetech",
+      "Software Engineer",
+      "Web Developer",
+      "Freelancer",
+      "Wordpress Developer",
+      "Leet-Coder",
+      "Student",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    onLoopDone: () => console.log(`loop completed after 3 runs.`),
+  });
+
+  return (
+    <div className="Corebackground bg-amber-700 relative min-h-screen">
+      <div className="prof h-screen sticky top-0 left-0 bg-black flex flex-row items-center">
+        <div className="h-2/3 font-bold w-1/2 flex flex-col">
+          <motion.h1
+            style={{ scale: titleScale }}
+            className="font-extrabold font-mono text-7xl -mt-12 text-white ml-11"
+          >
+            Hi!
+          </motion.h1>
+          <motion.h1
+            style={{ scale: titleScale, rotate: titleRotation }}
+            className="font-bold font-mono text-4xl mt-5 ml-15 text-white"
+          >
+            I'm Faseeh Ahmad
+          </motion.h1>
+          <motion.h1
+            style={{ opacity: textOpacity }}
+            className="font-bold font-mono text-3xl mt-3 ml-15 text-cyan-300"
+          >
+            {effectText}
+          </motion.h1>
+        </div>
+
+        <div className="h-2/3 font-bold w-1/2 flex flex-col items-center justify-center">
+          <motion.div
+            // initial = {{x:0, y: 0}}
+            style={{ x, y }}
+            // animate = {{left:xi , top: yi}}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="rounded-full mt-10"
+              src="/profile.jpg"
+              width={250}
+              height={250}
+              alt="background image"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          </motion.div>
+          <motion.p
+            style={{ opacity: textOpacity }}
+            className="mb-4 mt-8 font-bold text-2xl font-mono text-white"
           >
-            Read our docs
-          </a>
+            CEO Of Techpine
+          </motion.p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <motion.div
+        ref={beautyRef}
+        initial={{ opacity: 0, y: 100 }}
+        animate={beautyInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+        transition={{ duration: 0.5 }}
+        className="Beauty absolute bottom-0 w-full p-8"
+      >
+        <h2 className="text-4xl font-bold text-white text-center">The End !</h2>
+      </motion.div>
     </div>
   );
 }
